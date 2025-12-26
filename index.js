0cc75f2ae4816cf638aad34c278b357e997d0b53
YunPucne <yunpucne1@gmail.com>
2025-12-26 18:03:41 +0700

    Add files via upload
    initial bot

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits, Events } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
+  ],
+});
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`✅ Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.MessageCreate, async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.toLowerCase() === "ping") {
+    await message.reply("pong");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
